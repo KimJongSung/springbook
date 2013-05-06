aab1c62633e8450ba801dc77fb1c913a8c1cc0fe
JongSungKim <JongSungKim@192.168.2.3>
2013-05-06 22:18:40 +0900

    userdao,userDaoTest

---
+++ src/main/java/springbook/user/dao/userDaoTest.java
@@ -0,0 +1,36 @@
+package springbook.user.dao;
+
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class userDaoTest {
+
+	/**
+	 * @param args
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		UserDao dao = new UserDao();
+		User user = new User();
+		
+		user.setId("whiteship");
+		user.setName("KimJongsung");
+		user.setPassword("Password");
+		
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " update complete");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + "조회 성공");
+
+	}
+
+}
