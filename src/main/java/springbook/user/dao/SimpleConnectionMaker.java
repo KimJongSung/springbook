c75999c8566c16e680339b5cd3979b4d262c16bb
JongSungKim <JongSungKim@192.168.2.3>
2013-05-09 23:49:35 +0900

    인터페이스를 통한 디비 커낵션 설정

--- src/main/java/springbook/user/dao/SimpleConnectionMaker.java
+++ src/main/java/springbook/user/dao/SimpleConnectionMaker.java
@@ -4,14 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-
-	public Connection makeNewConnection() throws ClassNotFoundException,
+public class SimpleConnectionMaker implements ConnectionMaker{
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException,
 			SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1/springbook","spring","book");
 		
 		return c;
 	}
-
 }
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -11,7 +11,7 @@ import springbook.user.domain.User;
 public class UserDao {
 
 	public void add(User user) throws ClassNotFoundException,SQLException{
-		Connection c = simpleConnectionMaker.makeNewConnection();
+		Connection c = simpleConnectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
 		ps.setString(1,user.getId());
@@ -26,7 +26,7 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException,SQLException{
-		Connection c = simpleConnectionMaker.makeNewConnection();
+		Connection c = simpleConnectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		
